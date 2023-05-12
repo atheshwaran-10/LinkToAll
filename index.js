1a79dceb74269b233c8f7a84d513c13418af90b4
Atheshwaran K <athesh85@gmail.com>
2023-05-12 15:23:34 +0530

    link_update

---
+++ index.js
@@ -0,0 +1,56 @@
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".coc").addEventListener("click", function() {
+          window.open("https://www.codechef.com/users/athesh");
+          
+  });
+});
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".leetf").addEventListener("click", function() {
+          window.open("https://leetcode.com/atheshwarank21cs/");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".gfg").addEventListener("click", function() {
+          window.open("https://auth.geeksforgeeks.org/user/atheshwarank21cs");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".cf").addEventListener("click", function() {
+          window.open("https://codeforces.com/profile/atheshwarank21cs");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".gitt").addEventListener("click", function() {
+          window.open("https://github.com/atheshwaran-10");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".stt").addEventListener("click", function() {
+          window.open("https://stackoverflow.com/users/21794646/atheshwaran-k");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".link").addEventListener("click", function() {
+          window.open("https://www.linkedin.com/in/atheshwaran/");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  document.querySelector(".port").addEventListener("click", function() {
+          window.open("https://atheshwaran-10.github.io/Portfolio-Website/");
+  });
+});
